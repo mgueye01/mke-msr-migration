6b2823f3d2b06787bb6a5dc86a6fd9cd23b6ee71
Avinash Reddy Desireddy <avinashr.desireddy@gmail.com>
2021-06-23 00:14:07 -0500

    Support storing result to files & tags info

--- msr_repo_info.sh
+++ msr_repo_info.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-function status (){
-  echo " Usage: $0 <repo-file-name>"
-  exit 1
-}
+## Capture MSR Info
+[ -z "$MSR_HOSTNAME" ] && read -p "Enter the MSR hostname and press [ENTER]:" MSR_HOSTNAME
+[ -z "$MSR_USER" ] && read -p "Enter the MSR username and press [ENTER]:" MSR_USER
+[ -z "$MSR_PASSWORD" ] && read -s -p "Enter the MSR token or password and press [ENTER]:" MSR_PASSWORD
 
-if [ -z "$1" ]
-  then
-    status
-fi
-## Capture DTR Info
-[ -z "$DTR_HOSTNAME" ] && read -p "Enter the DTR hostname and press [ENTER]:" DTR_HOSTNAME
-[ -z "$DTR_USER" ] && read -p "Enter the DTR username and press [ENTER]:" DTR_USER
-[ -z "$DTR_PASSWORD" ] && read -s -p "Enter the DTR token or password and press [ENTER]:" DTR_PASSWORD
+echo "***************************************\\n"
+[ -z "$REPOS_FILE" ] && read -p "Repositories file(repositories.json):" REPOS_FILE
+[ -z "$REPOS_WITH_TAGS" ] && read -p "Repositories with tags file(repo_tags.txt):" REPOS_WITH_TAGS
+[ -z "$REPOS_COUNT_FILE" ] && read -s -p "Repository Count file(repo_count.txt):" REPOS_COUNT_FILE
 echo "***************************************\\n"
 
-RESULTS_FILE=$1
-: > $RESULTS_FILE
+REPOS_FILE=${1:-repositories.json}
+REPOS_WITH_TAGS=${2:-repo_tags.txt}
+REPOS_COUNT_FILE=${3:-repo_count.txt}
+
+## Empty files
+: > $REPOS_FILE
+: > $REPOS_WITH_TAGS
+: > $REPOS_COUNT_FILE
 
 ## Extract repositories info
-repos=$(curl -ks -u ${DTR_USER}:${DTR_PASSWORD} -X GET "https://${DTR_HOSTNAME}/api/v0/repositories?pageSize=100000&count=true" -H "accept: application/json" | jq -r -c .repositories)
+repos=$(curl -ks -u ${MSR_USER}:${MSR_PASSWORD} -X GET "https://${MSR_HOSTNAME}/api/v0/repositories?pageSize=100000&count=true" -H "accept: application/json" | jq .repositories)
 repo_num=$(echo $repos | jq 'length')
 repo_list=$(echo "${repos}" | jq -c -r '.[]')
 # # Loop through repos to get total tags
@@ -27,14 +29,26 @@ tags=0
 while IFS= read -r row ; do
     namespace=$(echo "$row" | jq -r .namespace)
     reponame=$(echo "$row" | jq -r .name)
-    tag_list=$(curl -ksLS -u ${DTR_USER}:${DTR_PASSWORD} -X GET "https://$DTR_HOSTNAME/api/v0/repositories/${namespace}/${reponame}/tags?pageSize=10000000")
+    tag_list=$(curl -ksLS -u ${MSR_USER}:${MSR_PASSWORD} -X GET "https://$MSR_HOSTNAME/api/v0/repositories/${namespace}/${reponame}/tags?pageSize=10000000")
     tag_count=$(echo "$tag_list" | jq 'length' )
+    tag_names=$(echo "$tag_list" | jq -r .[].name)
+    
+    if ((tag_count > 0)); then
+      for tag in $tag_names;
+      do
+        echo "${namespace}/${reponame}:${tag}" >> $REPOS_WITH_TAGS
+      done
+    fi
+
     echo "Org: ${namespace}, Repo: ${reponame}, Tags: ${tag_count}"
+    echo "Org: ${namespace}, Repo: ${reponame}, Tags: ${tag_count}" >> $REPOS_COUNT_FILE
+    
     tags=$(($tags + $tag_count))
 done <<< "$repo_list"
-echo "=========================================\\n"
-echo "Total Repos: ${repo_num}"
-echo "Total Tags: ${tags}"
 
-echo "Saving results to ${RESULTS_FILE}"
-echo $repos > ${RESULTS_FILE}
+echo "========================================="  >> $REPOS_COUNT_FILE
+echo "Total Repos: ${repo_num}" >> $REPOS_COUNT_FILE
+echo "Total Tags: ${tags}" >> $REPOS_COUNT_FILE
+
+echo "Saving results to ${REPOS_FILE}, ${REPOS_WITH_TAGS}, ${REPOS_COUNT_FILE}"
+echo $repos > ${REPOS_FILE}
