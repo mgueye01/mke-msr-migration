319fb568a9e42184b1e80f8d6a389d216311f3a0
Anoop Kumar <hello2anoop@gmail.com>
2021-06-26 13:19:38 -0400

    Added processing messages

--- msr_user_info.sh
+++ msr_user_info.sh
@@ -5,6 +5,7 @@
 [ -z "$MSR_USER" ] && read -p "Enter the OLD MSR username and press [ENTER]: " MSR_USER
 [ -z "$MSR_PASSWORD" ] && read -s -p "Enter the OLD MSR token or password and press [ENTER]: " MSR_PASSWORD
 
+echo -e "\nProcessing ....."
 MEMBERS_FILE=/var/tmp/dtr-members-$$
 ADMINS_FILE=/var/tmp/dtr-admins-$$
 
@@ -21,3 +22,7 @@ while IFS= read -r namespace ; do
   [ "[]" == $member_list ] || echo "$namespace: $member_list" >> $MEMBERS_FILE
   [ "[]" == $admin_list ] ||  echo "$namespace: $admin_list" >> $ADMINS_FILE
 done <<< "$nss"
+
+echo -e "====== PROCESSING COMPLETE ======"
+echo -e "Check the contents of the files:\n $MEMBERS_FILE and $ADMINS_FILE"
+echo -e "=================================\n"
