604d59c00fa6e7c0328a543cfa96d1c15fe8f275
Anoop Kumar <hello2anoop@gmail.com>
2021-07-03 22:30:56 -0400

    processing users - poc

--- msr_fix_admins.sh
+++ msr_fix_admins.sh
@@ -7,8 +7,6 @@ admins=$(cat old-dtr-admins)
 while IFS= read -r line ; do
   ns=$(echo $line | awk -F': [[]"|","' '{sub(/"]$/,""); print $1}')
   echo "namespace: $ns"
-  echo "====="
-  echo $line | awk -F': [[]"|","' '{sub(/"]$/,""); for (i=2; i<=NF; i++) print $i}' | xargs -I{} echo "hello {} is part of $ns"
-
-  echo "====="
+  echo $line | awk -F': [[]"|","' '{sub(/"]$/,""); for (i=2; i<=NF; i++) print $i}' | \
+    xargs -I{} echo "hello {} is part of $ns"
 done <<< "$admins"
