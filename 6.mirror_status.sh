5f7751dd403bede70ce1f53a9c57b03d6cf790ee
Avinash Desireddy <avinashr.desireddy@gmail.com>
2022-06-08 00:48:45 -0500

    Validate with remote repo
    - Validate with Remote Repo
    - Change output formatting.

--- 6.mirror_status.sh
+++ 6.mirror_status.sh
@@ -9,12 +9,17 @@ fi
 [ -z "$MSR_HOSTNAME" ] && read -p "Enter the MSR hostname and press [ENTER]:" MSR_HOSTNAME
 [ -z "$MSR_USER" ] && read -p "Enter the MSR username and press [ENTER]:" MSR_USER
 [ -z "$MSR_PASSWORD" ] && read -s -p "Enter the MSR token or password and press [ENTER]:" MSR_PASSWORD
+echo "********* REMOTE MSR CONFIG - Location to pull images from (Old MSR) ***********\\n"
+[ -z "$REMOTE_MSR_HOSTNAME" ] && read -p "Enter the REMOTE MSR hostname and press [ENTER]:" REMOTE_MSR_HOSTNAME
+[ -z "$REMOTE_MSR_USER" ] && read -p "Enter the MSR username and press [ENTER]:" REMOTE_MSR_USER
+[ -z "$REMOTE_MSR_PASSWORD" ] && read -s -p "Enter the MSR token or password and press [ENTER]:" REMOTE_MSR_PASSWORD
 echo ""
 echo "***************************************\\n"
 
 [ -z "$REPO_FILE" ] && read -p "Repositories file(repositories.json):" REPO_FILE
 
 CURLOPTS=(-kLsS -u ${MSR_USER}:${MSR_PASSWORD} -H 'accept: application/json' -H 'content-type: application/json')
+REMOTE_CURLOPTS=(-kLsS -u ${REMOTE_MSR_USER}:${REMOTE_MSR_PASSWORD} -H 'accept: application/json' -H 'content-type: application/json')
 
 ## Read repositories file
 repo_list=$(cat ${REPO_FILE} | jq -c -r '.[]') 
@@ -24,6 +29,17 @@ while IFS= read -r row ; do
     namespace=$(echo "$row" | jq -r .namespace)
     reponame=$(echo "$row" | jq -r .name)
     status="Not Enabled"
+    tag_count=0
+    remote_tag_count=0
+
+    ## Get Tags from Source MSR
+    tags=$(curl "${CURLOPTS[@]}" -X GET \
+        "https://${MSR_HOSTNAME}/api/v0/repositories/${namespace}/${reponame}/tags?pageSize=10000000")
+    tag_count=$(echo "$tags" | jq 'length' )
+    ## Get Tags from Destination MSR
+    tags_remote=$(curl "${REMOTE_CURLOPTS[@]}" -X GET \
+        "https://${REMOTE_MSR_HOSTNAME}/api/v0/repositories/${namespace}/${reponame}/tags?pageSize=10000000")
+    remote_tag_count=$(echo "$tags_remote" | jq 'length' )
 
     ## Get existing mirroring policies
     pollMirroringPolicies=$(curl "${CURLOPTS[@]}" -X GET \
@@ -37,9 +53,16 @@ while IFS= read -r row ; do
         if [ "x$enabled" == "xtrue" ]
         then
             lastStatus=$(echo $policy | jq -r .lastStatus.code)
-            echo "Repo: ${namespace}/${reponame}, PolicyId: ${id}, Enabled: ${enabled} ==> Status: ${lastStatus}"
+            if [ "x$remote_tag_count" == "x0" ]
+            then
+                lastStatus="SUCCESS"
+            fi
+            ## Repo, enabled, source_tags, destination_tags, last_status
+            echo "$namespace/$reponame,$enabled,$tag_count,$remote_tag_count,$lastStatus"
+            #echo "${namespace}/${reponame}, PolicyId: ${id}, Enabled: ${enabled} ==> Status: ${lastStatus}"
         else
-            echo "Repo: ${namespace}/${reponame}, PolicyId: ${id}, Enabled: ${enabled}"
+            echo "$namespace/$reponame,Mirror Not Enabled on this repository"
+            #echo "Repo: ${namespace}/${reponame}, PolicyId: ${id}, Enabled: ${enabled}"
         fi
         id=
         enabled=
